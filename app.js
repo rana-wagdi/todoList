79e08a655c09dbb988396412a933c2b23aec2e7d
rana2020701 <ranawagdi4@gmail.com>
2021-02-20 19:57:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const bodyParser = require("body-Parser");
 const app = express();
 
 app.get("/", function(req, res){
-    res.send("HELLO");
+
+    var today = new Date();
+
+    if (today.getDay() === 6 || today.getDay() === 0){
+        res.send("Yay! it's the weekend")
+    }else {
+        res.send("Boo! I have to work !")
+    }
 })
 
 app.listen(3000, function(){
