4660a5639ddf3eba3fd14378a70ec418fb8dafa5
rana2020701 <73128713+rana2020701@users.noreply.github.com>
2021-04-09 11:42:15 -0700

    refactor

--- app.js
+++ app.js
@@ -134,12 +134,8 @@ app.get("/work", function(req, res){
     res.render("list", {listTitle: "Work List", newListItems: workItems})
 })
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
 
-app.listen(port, function(){
+
+app.listen(3000, function(){
     console.log("Server is running on port 3000")
 })
